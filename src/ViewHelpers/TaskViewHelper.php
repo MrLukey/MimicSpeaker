b755b8014376f6d7739cbdf9b736544bd914f931
Luke Landau <luke.a.landau@gmail.com>
2021-09-25 22:07:41 +0100

    Improved code quality and added styling to tasks

--- src/ViewHelpers/TaskViewHelper.php
+++ src/ViewHelpers/TaskViewHelper.php
@@ -8,25 +8,60 @@ class TaskViewHelper
 {
 	public static function createIncompleteTaskListing(TaskEntityAbstract $task): string
 	{
-		return '<div class="task incomplete"><p class="taskText">' . $task->getText() .'</p>' .
-			'<p class="dateTime">Created: ' . $task->getCreationTime() .'</p>' .
-			'<form action="/complete" method="post"><input type="hidden" name="task' . $task->getID() .
-			'"><input type="submit" value="Complete"></form></div>';
+		return
+			'<div class="task incomplete">' .
+				'<p class="taskText">' . $task->getText() .'</p>' .
+				'<div class="controlWrapper">' .
+					'<p class="dateTime">Created: ' . $task->getCreationTime() .'</p>' .
+					'<div class="buttonWrapper">' .
+						'<form action="/complete" method="post">' .
+							'<input type="hidden" name="task' . $task->getID() . '">' .
+							'<input type="submit" value="Complete">' .
+						'</form>' .
+					'</div>' .
+				'</div>' .
+			'</div>';
 	}
 
 	public static function createCompletedTaskListing(TaskEntityAbstract $task): string
 	{
-		return '<div class="task complete"><p class="taskText">' . $task->getText() .'</p>' .
-			'<p class="dateTime">Completed: ' . $task->getCompletionTime() .'</p>' .
-			'<form action="/incomplete" method="post"><input type="hidden" name="task' . $task->getID() . '"><input type="submit" value="Mark Incomplete"></form>' .
-			'<form action="/delete" method="post"><input type="hidden" name="task' . $task->getID() . '"><input type="submit" value="Delete"></form></div>';
+		return
+			'<div class="task complete">' .
+				'<p class="taskText">' . $task->getText() .'</p>' .
+				'<div class="controlWrapper">' .
+					'<p class="dateTime">Completed: ' . $task->getCompletionTime() .'</p>' .
+					'<div class="buttonWrapper">' .
+						'<form action="/incomplete" method="post">' .
+							'<input type="hidden" name="task' . $task->getID() . '">' .
+							'<input type="submit" value="Mark Incomplete" class="editButton">' .
+						'</form>' .
+						'<form action="/delete" method="post">' .
+							'<input type="hidden" name="task' . $task->getID() . '">' .
+							'<input type="submit" value="Delete">' .
+						'</form>' .
+					'</div>' .
+				'</div>' .
+			'</div>';
 	}
 
 	public static function createDeletedTaskListing(TaskEntityAbstract $task): string
 	{
-		return '<div class="task deleted"><p class="taskText">' . $task->getText() .'</p>' .
-			'<p class="dateTime">Deleted: ' . $task->getDeletionTime() .'</p>' .
-			'<form action="/recover" method="post"><input type="hidden" name="task' . $task->getID() . '"><input type="submit" value="Restore"></form>' .
-			'<form action="/deletePermanently" method="post"><input type="hidden" name="task' . $task->getID() . '"><input type="submit" value="Delete Permanently"></form></div>';
+		return
+			'<div class="task deleted">' .
+				'<p class="taskText">' . $task->getText() .'</p>' .
+				'<div class="controlWrapper">' .
+					'<p class="dateTime">Deleted: ' . $task->getDeletionTime() .'</p>' .
+					'<div class="buttonWrapper">' .
+						'<form action="/recover" method="post">' .
+							'<input type="hidden" name="task' . $task->getID() . '">' .
+							'<input type="submit" value="Restore">' .
+						'</form>' .
+						'<form action="/deletePermanently" method="post">' .
+							'<input type="hidden" name="task' . $task->getID() . '">' .
+							'<input type="submit" value="Delete Permanently">' .
+						'</form>' .
+					'</div>' .
+				'</div>' .
+			'</div>';
 	}
 }
